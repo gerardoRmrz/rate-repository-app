8ca04e07fc1978673600595e605fd7c889b1f5a3
gerardoRmrz <grmzrso@outlook.com>
2026-08-25 16:45:36 -0500

    The reviewed repositories list has been rendered without styles

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,27 @@
+import { View, Text } from "react-native";
+
+/* id: string;
+    fullName: string;
+    description: string;
+    language: string;
+    forksCount: number;
+    stargazersCount: number;
+    ratingAverage: number;
+    reviewCount: number;
+    ownerAvatarUrl: string;
+ */
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
