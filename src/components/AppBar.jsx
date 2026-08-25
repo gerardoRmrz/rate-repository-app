d2db395ce1c62a955c6c1be368734f438845f00e
gerardoRmrz <grmzrso@outlook.com>
2026-08-25 17:26:08 -0500

    The app bar has been added

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import Constants from "expo-constants";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#1E1A4D",
+    height: 70,
+    justifyContent: "center",
+  },
+  text: {
+    color: "#F8FAFC",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable>
+        <Text style={styles.text}>Repositories</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default AppBar;
